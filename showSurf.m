dc443be46922fcc6749ec47fd346d9d09ac48899
Maciej Gryka <maciej.gryka@gmail.com>
2011-03-23 10:55:46 +0000

    added rgb2gray conversion in showSurf in case of 3-channel image

--- showSurf.m
+++ showSurf.m
@@ -1,5 +1,8 @@
 function showSurf(im)
     im = double(im);
+    if size(im,3) == 3
+        im = rgb2gray(im);
+    end
     surf(im, 'LineStyle', 'none');
     set(gca, 'Projection', 'Perspective');
     colormap jet;
